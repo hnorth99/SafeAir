5a5e1597227a5520efda7e0714efb57967e67594
hnorth99 <31296129+hnorth99@users.noreply.github.com>
2022-02-02 11:06:27 -0800

    Update SCD41RemoteReader.py

--- RasPiNode/SCD41RemoteReader.py
+++ RasPiNode/SCD41RemoteReader.py
@@ -1,18 +1,58 @@
-# Configuring Bluetooth device
+# Configuring Bluetooth devices
 # Open terminal
 # Run "hcitool scan"
-# Copy address next to HC-06
+# Copy address next to HC-06 on ArduinoMega/SCD41
+#       98:D3:91:FD:92:50
+# Copy address next to HC-06 on ArduinoNano/ResponseDevices
+#       TODO
 # Stop any running scripts
-# Run "sudo rfcomm connect hc10 98:D3:91:FD:92:50"
+# Run "sudo rfcomm connect hc10 <SCD41_DEV_ADDR>"
+# Run "sudo rfcomm connect hc10 <RESP_DEV_ADDR>"
 
 import serial
 import time
-scd41Port = serial.Serial("/dev/rfcomm0",baudrate=9600)
+import urllib.request
+import requests
+
+scd41Port = serial.Serial("/dev/rfcomm0", baudrate=9600)
+responseDevicesPort = serial.Serial("/dev/rfcomm1", baudrate=9600)
 
 while True:
-    scd41Port.write(str.encode('1')) #send '1' to Arduino
-    data=scd41Port.readline() #receive data from Arduino
-    if data:
-        print(data.decode('utf-8'))
-    time.sleep(0.1)
+    data = read_scd41()
+    if valid_data(data):
+        timestamp = time.local_time()
+        climate_response(data[0],  data[1], data[2])
+        push_data_to_thingspeak(timestamp, data[0],  data[1], data[2])
+    time.sleep(10)
 
+def read_scd41():
+    scd41Port.write(str.encode('1')) #send '1' to Arduino
+    serial_read = scd41Port.readline() #receive data from Arduino
+    # TODO: FORMAT SERIAL DATA INTO ARRAY
+    data = []
+    data.append() # TEMP
+    data.append() # HUMIDITY
+    data.append() # CO2
+    
+    return data
+
+def valid_data(data):
+    for num in data:
+        if (!isinstance(num, float)):
+            return false
+    return true
+
+def climate_response(temp,  humidity, co2):
+    response = ""
+    response += ((temp < 11.0) ? "1" : "0")
+    response += ((humidity < 40) ? "1" : "0")
+    response += ((humidity > 60) ? "1" : "0")
+    response += ((co2 > 1500) ? "1" : "0")
+    responseDevicesPort.write(str.encode(response))
+        
+def push_data_to_thingspeak(timestamp, temp,  humidity, co2):
+    url = "https://api.thingspeak.com/update?api_key="
+    key = "VZUNJG9HV7CDJAZC"
+    header = "&Time={}&Temperature={}&Humidity={}&Carbon_Dioxide={}".format(timestamp, temp, humidity, co2)
+    final_url = url+key+header
+    data = urllib.request.urlopen(final_url)
