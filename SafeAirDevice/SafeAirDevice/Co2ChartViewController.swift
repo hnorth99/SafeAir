7b1347c9470a1ec1362329272d8f5a6fcd82dd2f
Rafka <rafkadaou@Rafkas-MacBook-Air.local>
2022-02-28 16:52:13 -0800

    Added charts

---
+++ SafeAirDevice/SafeAirDevice/Co2ChartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Co2ChartViewController.swift
+//  SafeAirDevice
+//
+//  Created by Rafka Daou on 2/28/22.
+//
+
+import UIKit
+
+class Co2ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
